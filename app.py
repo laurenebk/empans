4f9a920033a4e49266156fc2a45d1f7cc2ee9773
laurenebk <laurene.borowik@gmail.com>
2026-06-05 15:01:30 +0200

    Update app.py

--- app.py
+++ app.py
@@ -9,14 +9,13 @@ with tab1:
     st.write("Empan de chiffres endroit")
 
     for empan in range(2, 10):
-    col1, col2, col3 = st.columns([1, 2, 2])
-    with col1:
-        st.write(f"**{empan} chiffres**")
-    with col2:
-        st.segmented_control(".", ["✅", "❌"], key=f"endroit_e1_{empan}", label_visibility="collapsed")
-    with col3:
-        st.segmented_control(".", ["✅", "❌"], key=f"endroit_e2_{empan}", label_visibility="collapsed")
-
+        col1, col2, col3 = st.columns([1, 2, 2])  # ← indenté
+        with col1:
+            st.write(f"**{empan} chiffres**")
+        with col2:
+            st.segmented_control(".", ["✅", "❌"], key=f"endroit_e1_{empan}", label_visibility="collapsed")
+        with col3:
+            st.segmented_control(".", ["✅", "❌"], key=f"endroit_e2_{empan}", label_visibility="collapsed")
 with tab2:
     st.write("Empan de chiffres inverse")
 
