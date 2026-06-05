7198534d3225b2b8ae2bc15fcf620287dca2d002
laurenebk <laurene.borowik@gmail.com>
2026-06-05 11:18:25 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,3 +2,14 @@ import streamlit as st
 
 st.title("Empans")
 st.write("Lire chaque suite de chiffre au rythme de un par seconde.")
+
+tab1, tab2, tab3 = st.tabs(["🎯 Épreuve", "📊 Scores", "📄 Rapport"])
+
+with tab1:
+    st.subheader("🎯 Épreuve")
+
+with tab2:
+    st.subheader("📊 Scores")
+
+with tab3:
+    st.subheader("📄 Rapport)
