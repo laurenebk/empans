0647346bf7c3191a3ca233b07db9c1c30ba7cc3a
laurenebk <laurene.borowik@gmail.com>
2026-06-05 15:09:55 +0200

    Update app.py

--- app.py
+++ app.py
@@ -8,14 +8,26 @@ tab1, tab2, tab3, tab4= st.tabs(["➡️ Empan endroit", "⬅️ Empan inverse",
 with tab1:
     st.write("Empan de chiffres endroit")
 
-    for empan in range(2, 10):
-        col1, col2, col3 = st.columns([1, 2, 2])  # ← indenté
+    sequences_endroit = {
+        2: [("9 - 7", "6 - 3")],
+        3: [("5 - 8 - 2", "6 - 9 - 4")],
+        4: [("7 - 2 - 8 - 6","6 - 4 - 3 - 9")]
+        5: [("4 - 2 - 7 - 3 - 1","7 - 5 - 8 - 3 - 6")]
+        6: [("3 - 9 - 2 - 4 - 8 - 7","6 - 1 - 9 - 4 - 7 - 3")]
+        7: [("4 - 1 - 7 - 9 - 3 - 8 - 6","6 - 9 - 1 - 7 - 4 - 2 - 8")]
+        8: [("3 - 8 - 2 - 9 - 6 - 1 - 7 - 4","5 - 8 - 1 - 3 - 2 - 6 - 4 - 7")]
+        9: [("2 - 7 - 5 - 8 - 6 - 3 - 1 - 9 - 4","7 - 1 - 3 - 9 - 4 - 2 - 5 - 6 - 8")]
+    }
+
+    for empan, (essai1, essai2) in sequences_endroit.items():
+        col1, col2, col3 = st.columns([1, 2, 2])
         with col1:
-            st.write(f"**{empan} chiffres**")
+            st.write(f"**{essai1}**")
         with col2:
-            st.segmented_control(".", ["✅", "❌"], key=f"endroit_e1_{empan}", label_visibility="collapsed")
+            st.segmented_control(".", ["Vrai", "Faux"], key=f"endroit_e1_{empan}", label_visibility="collapsed")
         with col3:
-            st.segmented_control(".", ["✅", "❌"], key=f"endroit_e2_{empan}", label_visibility="collapsed")
+            st.segmented_control(".", ["Vrai", "Faux"], key=f"endroit_e2_{empan}", label_visibility="collapsed")
+
 with tab2:
     st.write("Empan de chiffres inverse")
 
